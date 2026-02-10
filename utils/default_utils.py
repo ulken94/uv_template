6569c03c763a069280a81933db8a037fa609f783
Haneol Kim <bestwook7@gmail.com>
2026-02-10 20:16:40 +0900

    Update readme
    * Add how to generate documentation in README
    
    * Add default utility module

---
+++ utils/default_utils.py
@@ -0,0 +1,17 @@
+"""Default utility example module.
+
+-Author: ulken94
+-Contact: bestwook7@gmail.com
+"""
+
+
+def greet(name: str) -> None:
+    """Greets a person with their name.
+
+    Args:
+        name (str): The name of the person to greet.
+
+    Returns:
+        None
+    """
+    print(f"Hello, {name}!")
